fff0ec1bae248f807a49bd222b6a1fc99de52ef4
Echo2f13 <kotamanishdev05@gmail.com>
2025-05-12 17:40:59 +0530

    changed email app password

--- ewaste/settings.py
+++ ewaste/settings.py
@@ -141,4 +141,4 @@ EMAIL_USE_TLS = 'True'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'ecoearn3@gmail.com'
-EMAIL_HOST_PASSWORD = 'oqzw unps sayf aweu'
+EMAIL_HOST_PASSWORD = 'wpdn fsib zuol tkcd'
